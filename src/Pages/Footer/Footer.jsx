d2f68c815bad0a9f5b3be979fb48f0a110d48a96
Tofazzal26 <afranislamabir6789@gmail.com>
2024-05-10 17:51:50 +0600

    footer style finish

--- src/Pages/Footer/Footer.jsx
+++ src/Pages/Footer/Footer.jsx
@@ -6,40 +6,35 @@ const Footer = () => {
       <footer className="p-10 bg-base-200 text-base-content">
         <div className="footer container mx-auto">
           <nav className="space-y-2">
-            <h6 className="text-black text-xl font-semibold">Products</h6>
+            <h6 className="text-black text-xl font-semibold">Location</h6>
             <a className="link link-hover font-semibold text-[14px]">
-              Prices Drop
+              Barcelona
+            </a>
+            <a className="link link-hover font-semibold text-[14px]">London</a>
+            <a className="link link-hover font-semibold text-[14px]">
+              New Work
+            </a>
+            <a className="link link-hover font-semibold text-[14px]">Paris</a>
+          </nav>
+          <nav className="space-y-2">
+            <h6 className="text-black text-xl font-semibold">Social</h6>
+            <a className="link link-hover font-semibold text-[14px]">
+              Instagram
             </a>
             <a className="link link-hover font-semibold text-[14px]">
-              New Products
+              Facebook
             </a>
+            <a className="link link-hover font-semibold text-[14px]">Twitter</a>
             <a className="link link-hover font-semibold text-[14px]">
-              Best Sales
-            </a>
-            <a className="link link-hover font-semibold text-[14px]">
-              Contact Us
+              Linkdein
             </a>
           </nav>
           <nav className="space-y-2">
-            <h6 className="text-black text-xl font-semibold">Login</h6>
-            <a className="link link-hover font-semibold text-[14px]">Stores</a>
-            <a className="link link-hover font-semibold text-[14px]">Login</a>
+            <h6 className="text-black text-xl font-semibold">Language</h6>
+            <a className="link link-hover font-semibold text-[14px]">English</a>
+            <a className="link link-hover font-semibold text-[14px]">France</a>
             <a className="link link-hover font-semibold text-[14px]">
-              Contact Us
-            </a>
-            <a className="link link-hover font-semibold text-[14px]">
-              My Account
-            </a>
-          </nav>
-          <nav className="space-y-2">
-            <h6 className="text-black text-xl font-semibold">Your Account</h6>
-            <a className="link link-hover font-semibold text-[14px]">
-              Personal Info
-            </a>
-            <a className="link link-hover font-semibold text-[14px]">Orders</a>
-            <a className="link link-hover font-semibold text-[14px]">Login</a>
-            <a className="link link-hover font-semibold text-[14px]">
-              Addresses
+              United State
             </a>
           </nav>
           <form className="space-y-2">
